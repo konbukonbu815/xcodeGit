44d75766b8923ee09a2185387ab8416e2fa6bbbf
若狭　健太 <wakasakenta@wakasakenta-no-macbook-pro.local>
2017-04-23 18:15:52 +0900

    Initial Commit

---
+++ wave2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wave2
+//
+//  Created by 若狭　健太 on 2017/04/23.
+//  Copyright © 2017年 wakasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ wave2Tests/wave2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  wave2Tests.swift
+//  wave2Tests
+//
+//  Created by 若狭　健太 on 2017/04/23.
+//  Copyright © 2017年 wakasa. All rights reserved.
+//
+
+import XCTest
+@testable import wave2
+
+class wave2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
